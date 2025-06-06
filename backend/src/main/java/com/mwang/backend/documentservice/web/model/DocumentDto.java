3f4a5f74bc8929850c498a3022a5797bf1239b52
Mason Wang <mason123wp@gmail.com>
2025-06-06 02:52:48 -0400

    Change id field type in DocumentDto from String to UUID

--- backend/src/main/java/com/mwang/backend/documentservice/web/model/DocumentDto.java
+++ backend/src/main/java/com/mwang/backend/documentservice/web/model/DocumentDto.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class DocumentDto {
-    private String id;
+    private UUID id;
     private String title;
     private String content;
 }
