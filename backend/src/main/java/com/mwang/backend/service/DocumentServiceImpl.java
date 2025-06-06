ee31b2441a55294899cee0673addcdb6adbade59
Mason Wang <mason123wp@gmail.com>
2025-06-06 00:00:14 -0400

    Add DocumentServiceImpl implementation with repository and mapper integration

--- backend/src/main/java/com/mwang/backend/service/DocumentServiceImpl.java
+++ backend/src/main/java/com/mwang/backend/service/DocumentServiceImpl.java
@@ -1,14 +1,22 @@
 package com.mwang.backend.service;
 
+import com.mwang.backend.repository.DocumentRepository;
+import com.mwang.backend.web.mappers.DocumentMapper;
 import com.mwang.backend.web.model.DocumentDto;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@AllArgsConstructor
 @Service
 public class DocumentServiceImpl implements DocumentService {
+
+    private final DocumentRepository repository;
+    private final DocumentMapper mapper;
+
     @Override
     public DocumentDto create(DocumentDto documentDto) {
         return null;
