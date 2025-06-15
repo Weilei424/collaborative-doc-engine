b218ddfbc3d6e0bac56c0990fcfcb2a30b7d1e5a
Mason Wang <mason123wp@gmail.com>
2025-06-15 03:18:48 -0400

    Add WebSocket handler for document management

--- backend/src/main/java/com/mwang/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/mwang/backend/config/WebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.mwang.backend.config;
 
+import com.mwang.backend.websocket.DocumentWebSocketHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
+        registry.addHandler(new DocumentWebSocketHandler(), "/ws/documents")
+                .setAllowedOrigins("*"); 
     }
 }
