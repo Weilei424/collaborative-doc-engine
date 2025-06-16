ebf914f5372c154ee42e41e3199ba64b61c95ced
Mason Wang <mason123wp@gmail.com>
2025-06-16 02:28:14 -0400

    Add CollaborationMessage model for document collaboration events

--- backend/src/main/java/com/mwang/backend/websocket/DocumentWebSocketHandler.java
+++ backend/src/main/java/com/mwang/backend/websocket/DocumentWebSocketHandler.java
@@ -1,25 +1,33 @@
 package com.mwang.backend.websocket;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mwang.backend.web.model.CollaborationMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DocumentWebSocketHandler extends TextWebSocketHandler {
-    private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+    private static final Map<String, Set<WebSocketSession>> docSessions = new ConcurrentHashMap<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
-        sessions.add(session);
+        //todo
     }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        for (WebSocketSession s : sessions) {
+
+        CollaborationMessage msg = objectMapper.readValue(message.getPayload(), CollaborationMessage.class);
+        docSessions.computeIfAbsent(msg.getDocId(), k -> Collections.newSetFromMap(new ConcurrentHashMap<>())).add(session);
+
+        for (WebSocketSession s : docSessions.get(msg.getDocId())) {
             if (s.isOpen() && s != session) {
                 s.sendMessage(message);
             }
@@ -28,6 +36,8 @@ public class DocumentWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        sessions.remove(session);
+        for (Set<WebSocketSession> sessions : docSessions.values()) {
+            sessions.remove(session);
+        }
     }
 }
