b13eeb9d48ca452fb85d0d58dedcc13d2e60ecc7
Mason Wang <mason123wp@gmail.com>
2025-06-22 23:17:29 -0400

    Add JpaConfig class to enable JPA auditing

---
+++ backend/src/main/java/com/mwang/backend/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.mwang.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
