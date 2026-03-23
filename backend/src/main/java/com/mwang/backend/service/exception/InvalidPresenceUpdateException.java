83b14d71016ea15459b8a5cfa1429b2bf68a9eda
Mason Wang <mason123wp@gmail.com>
2026-03-23 19:30:24 -0400

    add invalid collaboration presence exception

---
+++ backend/src/main/java/com/mwang/backend/service/exception/InvalidPresenceUpdateException.java
@@ -0,0 +1,7 @@
+package com.mwang.backend.service.exception;
+
+public class InvalidPresenceUpdateException extends RuntimeException {
+    public InvalidPresenceUpdateException(String message) {
+        super(message);
+    }
+}
