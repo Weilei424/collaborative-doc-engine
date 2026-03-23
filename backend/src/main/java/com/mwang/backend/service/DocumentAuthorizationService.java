de3cd8bcf9b39830e0ebb4bd6e4254cf214a8e92
Mason Wang <mason123wp@gmail.com>
2026-03-22 21:30:58 -0400

    define document authorization contract for access rules

---
+++ backend/src/main/java/com/mwang/backend/service/DocumentAuthorizationService.java
@@ -0,0 +1,10 @@
+package com.mwang.backend.service;
+
+import com.mwang.backend.domain.Document;
+import com.mwang.backend.domain.User;
+
+public interface DocumentAuthorizationService {
+    void assertCanRead(Document document, User actor);
+    void assertOwner(Document document, User actor);
+    String resolveEffectivePermission(Document document, User actor);
+}
