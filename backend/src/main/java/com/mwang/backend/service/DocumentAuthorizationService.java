6fcad3a191b1f4a90e6aa6061fad054c58076f6c
Mason Wang <mason123wp@gmail.com>
2026-03-25 21:42:36 -0400

    add assertCanWrite to DocumentAuthorizationService interface

--- backend/src/main/java/com/mwang/backend/service/DocumentAuthorizationService.java
+++ backend/src/main/java/com/mwang/backend/service/DocumentAuthorizationService.java
@@ -5,6 +5,7 @@ import com.mwang.backend.domain.User;
 
 public interface DocumentAuthorizationService {
     void assertCanRead(Document document, User actor);
+    void assertCanWrite(Document document, User actor);
     void assertOwner(Document document, User actor);
     String resolveEffectivePermission(Document document, User actor);
 }
