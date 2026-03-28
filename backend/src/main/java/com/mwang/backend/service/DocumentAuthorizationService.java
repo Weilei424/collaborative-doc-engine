0101e494403fa3db9b990091863687d6beebb24e
Mason Wang <mason123wp@gmail.com>
2026-03-27 20:18:23 -0400

    add assertCanAdmin to interface

--- backend/src/main/java/com/mwang/backend/service/DocumentAuthorizationService.java
+++ backend/src/main/java/com/mwang/backend/service/DocumentAuthorizationService.java
@@ -7,5 +7,6 @@ public interface DocumentAuthorizationService {
     void assertCanRead(Document document, User actor);
     void assertCanWrite(Document document, User actor);
     void assertOwner(Document document, User actor);
+    void assertCanAdmin(Document document, User actor);
     String resolveEffectivePermission(Document document, User actor);
 }
