e566186cf89e0915c3a3a049bebcb4994a281f33
Mason Wang <mason123wp@gmail.com>
2026-03-28 09:21:34 -0400

    register MdcChannelInterceptor as first interceptor on client inbound channel

--- backend/src/main/java/com/mwang/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/mwang/backend/config/WebSocketConfig.java
@@ -60,7 +60,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new ChannelInterceptor() {
+        registration.interceptors(new MdcChannelInterceptor(), new ChannelInterceptor() {
             @Override
             public Message<?> preSend(Message<?> message, MessageChannel channel) {
                 StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
