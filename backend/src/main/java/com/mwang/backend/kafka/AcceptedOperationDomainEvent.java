51f01c3e42336e9cf1e64151fd748102a5f683ad
Mason Wang <mason123wp@gmail.com>
2026-04-23 17:50:49 -0400

    fix: deleted (retired fire-and-forget publisher)

--- backend/src/main/java/com/mwang/backend/kafka/AcceptedOperationDomainEvent.java
+++
@@ -1,8 +0,0 @@
-package com.mwang.backend.kafka;
-
-import com.mwang.backend.web.model.AcceptedOperationResponse;
-
-public record AcceptedOperationDomainEvent(
-        AcceptedOperationResponse response,
-        long baseVersion
-) {}
